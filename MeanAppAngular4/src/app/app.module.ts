7c64864f058d2e044acf8517b6348aa26ce25f79
Endri Azizi <endri.azizi@gmail.com>
2017-10-31 12:58:06 +0100

    fixing frontend routes

--- MeanAppAngular4/src/app/app.module.ts
+++ MeanAppAngular4/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+//Routes Module
+import {RouterModule, Routes} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 //new import component 
@@ -11,6 +14,15 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+
+
+const appRoutes: Routes = [
+{path:'', component: HomeComponent},
+{path:'register', component: RegisterComponent},
+{path:'login', component: LoginComponent},
+{path:'dashboard', component: DashboardComponent},
+{path:'profile', component: ProfileComponent}
+]
 
 @NgModule({
   declarations: [
@@ -26,7 +38,9 @@ import { ProfileComponent } from './components/profile/profile.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    //routes module
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
